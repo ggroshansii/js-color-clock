c65cb30e5dbf3268ae9b2468fa4735ba8ee243b9
ggroshansii <garthgroshansii@gmail.com>
2021-09-01 12:59:32 -0400

    refactored setInterval and it's callback function, getCurrentTime, to update the clockDisplay element every second

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,26 @@
 
 
+const clockDisplay = document.querySelector(".clock-display");
 
+
+
+
+//Obtains the current time in hours:minutes:seconds and logs it to the clockDisplay element
 function getCurrentTime() {
   const today = new Date();
-  let time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  console.log(time);
+  let seconds;
+  today.getSeconds() < 10 ? seconds = "0" + today.getSeconds() : seconds = today.getSeconds();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + seconds;
+  clockDisplay.textContent = time;
 }
 
+//Updates the clockDisplay with the current time every second
 setInterval(getCurrentTime, 1000);
+
+
+function getSecondsPercentage() {
+    const today = new Date();
+    console.log((today.getSeconds() / 60).toFixed(2));
+}
+
+setInterval(getSecondsPercentage, 1000);
