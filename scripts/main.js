f910c3e1fb6e28c494fc6a1d5b9afba0135bf6a0
ggroshansii <garthgroshansii@gmail.com>
2021-09-01 14:22:23 -0400

    refactored getCurrentTime() and setInterval functions due to error of not updating

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,54 @@
-
-
 const clockDisplay = document.querySelector(".clock-display");
-
-
-
 
 //Obtains the current time in hours:minutes:seconds and logs it to the clockDisplay element
 function getCurrentTime() {
   const today = new Date();
-  let seconds;
-  today.getSeconds() < 10 ? seconds = "0" + today.getSeconds() : seconds = today.getSeconds();
-  let time = today.getHours() + ":" + today.getMinutes() + ":" + seconds;
-  clockDisplay.textContent = time;
+  let seconds, hours;
+  today.getSeconds() < 10 ? (seconds = "0" + today.getSeconds()) : (seconds = today.getSeconds());
+  today.getHours() > 12 ? (hours = today.getHours() - 12) : hours = today.getHours();
+  return {
+    hours: hours,
+    minutes: today.getMinutes(),
+    seconds,
+  };
+}
+
+function updateTime() {
+    const hours = getCurrentTime().hours;
+    const minutes = getCurrentTime().minutes;
+    const seconds = getCurrentTime().seconds;
+    clockDisplay.textContent = hours + ":" + minutes + ":" + seconds;
+}
+
+function updateTimetoHex() {
+    const hours = getCurrentTime().hours.toString(16);
+    const minutes = getCurrentTime().minutes.toString(16);
+    const seconds = getCurrentTime().seconds.toString(16);
+    clockDisplay.textContent = hours + ":" + minutes + ":" + seconds;
 }
 
 //Updates the clockDisplay with the current time every second
-setInterval(getCurrentTime, 1000);
-
+setInterval(updateTime,1000);
 
 function updateProgressBarLength() {
-    const today = new Date();
-    let percentage = (today.getSeconds() / 60).toFixed(2);
-    const clockProgressBar = document.querySelector(".clock-progress-bar");
-    console.log(clockProgressBar);
-    clockProgressBar.style.width = (percentage * 100) + "%";
+  const today = new Date();
+  let percentage = (today.getSeconds() / 60).toFixed(2);
+  const clockProgressBar = document.querySelector(".clock-progress-bar");
+  clockProgressBar.style.width = percentage * 100 + "%";
 }
 
 setInterval(updateProgressBarLength, 1000);
 
 
-function convertToHex(hours, minutes, seconds) {
-    time = hours.toString(16) + ":" + minutes.toString(16) + ":" + seconds.toString(16)
-}
+const clockFace = document.querySelector(".clock-face");
 
+clockFace.addEventListener("mouseover", () => {
+    setInterval(updateTimetoHex,
+        1000
+      );
+});
+
+clockFace.addEventListener("mouseout", () => {
+    setInterval(updateTime, 1000
+      );
+});
